afc662ca389f12c18122b1fb99d54533f36fb3b5
Devtr0n <richardsmailbox@gmail.com>
2015-04-20 11:44:09 -0500

    initial upload of TacFinder service library solution

---
+++ TacoFinderServiceLib/Service1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ServiceModel.Web;
+
+namespace WcfJsonRestService
+{
+    public class Service1 : IService1
+    {
+        [WebInvoke(Method = "GET",
+                    ResponseFormat = WebMessageFormat.Json,
+                    UriTemplate = "data/{id}")]
+        public Taco GetTaco(string id)
+        {
+            // lookup taco with the requested id 
+            return new Taco()
+            {
+                Id = Convert.ToInt32(id),
+                Name = "Tacos Al Pastor",
+                TacoFillingType = 3,
+                TacoFillingSubType = 1,
+                TortillaType = 2,
+                TortillaShellType = 4,
+                SalsaType = 7,
+                TacoToppingsType = 3
+            };
+        }
+    }
+
+    public class Taco
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TacoFillingType { get; set; }
+        public int TacoFillingSubType { get; set; }
+        public int TortillaType { get; set; }
+        public int TortillaShellType { get; set; }
+        public int SalsaType { get; set; }
+        public int TacoToppingsType { get; set; }
+    }
+}
---
+++ TacoFinderServiceLib/TacoEnumUtil.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TacoFinderServiceLib
+{
+    class TacoEnumUtil
+    {
+    }
+
+    public enum TacoType : byte
+    {
+        Seafood = 1, AlPastor, AlCarbon, Gringo, Dessert, Fusion, Other
+    }
+
+    public enum TacoFillingType : byte
+    {
+        Meat = 1, Eggs, Veggie, Fish, Shrimp, Bean, Other
+    }
+
+    public enum TortillaType : byte
+    {
+        FlourHomemade = 1, FlourNonHomemade, YellowCorn, WhiteCorn, Hybrid, Other
+    }
+
+    public enum TortillaShellType : byte
+    {
+        Crispy = 1, Soft, Hard, Puffy, Other
+    }
+
+    public enum SalsaType : byte
+    {
+        Fresh = 1, PicoDeGallo, Verde, Tomatillo, Roja, Avocado, TacoSauce, Jarred, Other
+    }
+
+    public enum TacoToppingsType : byte
+    {
+        Cilantro = 1, WhiteOnion, Tomato, SourCream, Lettuce, RefriedBeans, ShreddedCheese, Jalapenos, Bacon, Chorizo, Sausage, Guacamole, Avocado, Olive, Mushroom, Corn, GreenOnion, Rice, Cabbage, GrilledOnions, Other
+    }
+
+    public enum TacoAvailablilityType : byte
+    {
+        Breakfast = 1, Lunch, Dinner, TwentyFourHour, Dessert, Other
+    }
+
+    public enum TacoLocationType : byte
+    {
+        Trailer = 1, Restaurant, Stand, Other
+    }
+
+    public enum UserType : byte
+    {
+        Regular = 1, Business, Admin, Other
+    }
+
+    public enum Rating : byte
+    {
+        Unacceptable = 1, JustAlright, Average, Good, VeryGood, Outstanding, Incredible
+    }	
+	
+}
